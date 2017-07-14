5c05643a2ccb8b41b9622ce7728ce80d4f381f56
VenoM <sebastianbrunner@hotmail.ch>
2017-07-14 13:08:09 +0200

    Add files via upload
    init

---
+++ WindowsFormsApplication1/Main.Designer.cs
@@ -0,0 +1,173 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Main
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Main));
+            this.btn_refresh = new System.Windows.Forms.Button();
+            this.lbl_leftScore = new System.Windows.Forms.Label();
+            this.lbl_scoreRight = new System.Windows.Forms.Label();
+            this.lbl_matchId = new System.Windows.Forms.Label();
+            this.btn_setNull = new System.Windows.Forms.Button();
+            this.nud_leftScore = new System.Windows.Forms.NumericUpDown();
+            this.nud_rightScore = new System.Windows.Forms.NumericUpDown();
+            this.nud_matchId = new System.Windows.Forms.NumericUpDown();
+            ((System.ComponentModel.ISupportInitialize)(this.nud_leftScore)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nud_rightScore)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nud_matchId)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btn_refresh
+            // 
+            this.btn_refresh.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+            this.btn_refresh.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_refresh.Location = new System.Drawing.Point(145, 226);
+            this.btn_refresh.Name = "btn_refresh";
+            this.btn_refresh.Size = new System.Drawing.Size(75, 23);
+            this.btn_refresh.TabIndex = 0;
+            this.btn_refresh.Text = "refresh";
+            this.btn_refresh.UseVisualStyleBackColor = true;
+            this.btn_refresh.Click += new System.EventHandler(this.btn_refresh_Click);
+            // 
+            // lbl_leftScore
+            // 
+            this.lbl_leftScore.AutoSize = true;
+            this.lbl_leftScore.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_leftScore.Location = new System.Drawing.Point(41, 13);
+            this.lbl_leftScore.Name = "lbl_leftScore";
+            this.lbl_leftScore.Size = new System.Drawing.Size(70, 19);
+            this.lbl_leftScore.TabIndex = 4;
+            this.lbl_leftScore.Text = "score left";
+            // 
+            // lbl_scoreRight
+            // 
+            this.lbl_scoreRight.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.lbl_scoreRight.AutoSize = true;
+            this.lbl_scoreRight.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_scoreRight.Location = new System.Drawing.Point(153, 13);
+            this.lbl_scoreRight.Name = "lbl_scoreRight";
+            this.lbl_scoreRight.Size = new System.Drawing.Size(78, 19);
+            this.lbl_scoreRight.TabIndex = 5;
+            this.lbl_scoreRight.Text = "score right";
+            // 
+            // lbl_matchId
+            // 
+            this.lbl_matchId.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+            this.lbl_matchId.AutoSize = true;
+            this.lbl_matchId.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_matchId.Location = new System.Drawing.Point(101, 179);
+            this.lbl_matchId.Name = "lbl_matchId";
+            this.lbl_matchId.Size = new System.Drawing.Size(68, 19);
+            this.lbl_matchId.TabIndex = 6;
+            this.lbl_matchId.Text = "match-ID";
+            // 
+            // btn_setNull
+            // 
+            this.btn_setNull.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+            this.btn_setNull.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_setNull.Location = new System.Drawing.Point(51, 226);
+            this.btn_setNull.Name = "btn_setNull";
+            this.btn_setNull.Size = new System.Drawing.Size(75, 23);
+            this.btn_setNull.TabIndex = 7;
+            this.btn_setNull.Text = "set zero";
+            this.btn_setNull.UseVisualStyleBackColor = true;
+            this.btn_setNull.Click += new System.EventHandler(this.btn_setNull_Click);
+            // 
+            // nud_leftScore
+            // 
+            this.nud_leftScore.Font = new System.Drawing.Font("Calibri", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.nud_leftScore.Location = new System.Drawing.Point(12, 35);
+            this.nud_leftScore.Name = "nud_leftScore";
+            this.nud_leftScore.Size = new System.Drawing.Size(114, 86);
+            this.nud_leftScore.TabIndex = 8;
+            this.nud_leftScore.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.nud_leftScore.UpDownAlign = System.Windows.Forms.LeftRightAlignment.Left;
+            this.nud_leftScore.ValueChanged += new System.EventHandler(this.nud_leftScore_ValueChanged);
+            // 
+            // nud_rightScore
+            // 
+            this.nud_rightScore.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.nud_rightScore.Font = new System.Drawing.Font("Calibri", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.nud_rightScore.Location = new System.Drawing.Point(138, 35);
+            this.nud_rightScore.Name = "nud_rightScore";
+            this.nud_rightScore.Size = new System.Drawing.Size(120, 86);
+            this.nud_rightScore.TabIndex = 9;
+            this.nud_rightScore.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.nud_rightScore.ValueChanged += new System.EventHandler(this.nud_rightScore_ValueChanged);
+            // 
+            // nud_matchId
+            // 
+            this.nud_matchId.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+            this.nud_matchId.Location = new System.Drawing.Point(78, 201);
+            this.nud_matchId.Name = "nud_matchId";
+            this.nud_matchId.Size = new System.Drawing.Size(120, 20);
+            this.nud_matchId.TabIndex = 10;
+            this.nud_matchId.ValueChanged += new System.EventHandler(this.nud_matchId_ValueChanged);
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::HeroesLoungeMatchHandler.Properties.Resources.heroes_lounge_transparent;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.Controls.Add(this.nud_matchId);
+            this.Controls.Add(this.nud_rightScore);
+            this.Controls.Add(this.nud_leftScore);
+            this.Controls.Add(this.btn_setNull);
+            this.Controls.Add(this.lbl_matchId);
+            this.Controls.Add(this.lbl_scoreRight);
+            this.Controls.Add(this.lbl_leftScore);
+            this.Controls.Add(this.btn_refresh);
+            this.DoubleBuffered = true;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MinimizeBox = false;
+            this.MinimumSize = new System.Drawing.Size(300, 300);
+            this.Name = "Main";
+            this.Text = "HeroesLoungeMatchHandler";
+            ((System.ComponentModel.ISupportInitialize)(this.nud_leftScore)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nud_rightScore)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nud_matchId)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn_refresh;
+        private System.Windows.Forms.Label lbl_leftScore;
+        private System.Windows.Forms.Label lbl_scoreRight;
+        private System.Windows.Forms.Label lbl_matchId;
+        private System.Windows.Forms.Button btn_setNull;
+        private System.Windows.Forms.NumericUpDown nud_leftScore;
+        private System.Windows.Forms.NumericUpDown nud_rightScore;
+        private System.Windows.Forms.NumericUpDown nud_matchId;
+    }
+}
+
---
+++ WindowsFormsApplication1/Main.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+/*
+ * Programmed with love for HeroesLounge. By VenoM
+ * */
+namespace WindowsFormsApplication1
+{
+    public partial class Main : Form
+    {
+        private int scoreLeft;
+        private int scoreRight;
+        private int matchId;
+
+        public Main()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// Writes the txt-files
+        /// </summary>
+        private void btn_refresh_Click(object sender, EventArgs e)
+        {
+            System.IO.File.WriteAllText("scoreLeft.txt", Convert.ToString(this.scoreLeft));
+            System.IO.File.WriteAllText("scoreRight.txt", Convert.ToString(this.scoreRight));
+            System.IO.File.WriteAllText("matchId.txt", Convert.ToString(this.matchId));
+        }
+
+        /// <summary>
+        /// sets the score to 0. Text files aren't automatic generated.
+        /// </summary>
+        private void btn_setNull_Click(object sender, EventArgs e)
+        {
+            nud_leftScore.Value = 0;
+            nud_rightScore.Value = 0;
+        }
+
+        /// <summary>
+        /// Setter-method for left score
+        /// </summary>
+        /// <param name="scoreLeft">the left score</param>
+        private void setScoreLeft(int scoreLeft) {
+            this.scoreLeft = scoreLeft;
+        }
+
+        /// <summary>
+        /// Setter-method for right score
+        /// </summary>
+        /// <param name="scoreRight">the right score</param>
+        private void setScoreRight(int scoreRight)
+        {
+            this.scoreRight = scoreRight;
+        }
+
+        /// <summary>
+        /// Setter-method for the match-id
+        /// </summary>
+        /// <param name="matchId">the match-ID</param>
+        private void setMatchId(int matchId)
+        {
+            this.matchId = matchId;
+        }
+
+        private void nud_leftScore_ValueChanged(object sender, EventArgs e)
+        {
+             setScoreLeft(Convert.ToInt32(nud_leftScore.Value));
+        }
+
+        private void nud_rightScore_ValueChanged(object sender, EventArgs e)
+        {
+            setScoreRight(Convert.ToInt32(nud_rightScore.Value));
+        }
+
+        private void nud_matchId_ValueChanged(object sender, EventArgs e)
+        {
+            setMatchId(Convert.ToInt32(nud_matchId.Value));
+        }
+    }
+}
