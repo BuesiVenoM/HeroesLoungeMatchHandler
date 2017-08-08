988492a82cc9dcab5d6e0115b60391c9244f9f4f
Sebastian Brunner <brunnse1@students.zhaw.ch>
2017-08-08 18:04:13 +0200

    refactored version

--- WindowsFormsApplication1/Main.cs
+++ WindowsFormsApplication1/Main.cs
@@ -20,10 +20,7 @@ namespace WindowsFormsApplication1
         private string teamRight = "";
         private string streamTitle = "";
         private string path = "";
-        
-        
-        
-
+     
         public Main()
         {
             path = Settings.Default.Output;
@@ -44,7 +41,6 @@ namespace WindowsFormsApplication1
             System.IO.File.WriteAllText(path + "teamLeft.txt", Convert.ToString(this.teamLeft));
             System.IO.File.WriteAllText(path + "teamRight.txt", Convert.ToString(this.teamRight));
             System.IO.File.WriteAllText(path + "streamTitle.txt", Convert.ToString(this.streamTitle));
-            
         }
 
         /// <summary>
@@ -110,34 +106,36 @@ namespace WindowsFormsApplication1
              * team-pic: https://heroeslounge.gg/api/v1/team/1/logo
              * */
 
-            string specteamdatal;
-            string specteampicdata;
-
+            /* creating a Webclient for Web-downloads */
             WebClient wc = new WebClient();
 
+            /* settings for the JSON-Serializer */
             var settings = new JsonSerializerSettings
             {
                 NullValueHandling = NullValueHandling.Ignore,
                 MissingMemberHandling = MissingMemberHandling.Ignore
             };
 
+            /* creating the urls based on the given match-id */
             String matchurl = "https://heroeslounge.gg/api/v1/matches/" + matchId;
             String matchteamsurl = matchurl + "/teams";
             
+            /* getting the Data from the Website*/
             var matchdata = JsonConvert.DeserializeObject<HeroesLoungeMatchHandler.Match>(wc.DownloadString(matchurl), settings);
-            var test = wc.DownloadString(matchteamsurl);
-            var matchteamsdata = JsonConvert.DeserializeObject<List<HeroesLoungeMatchHandler.Team>>(test, settings);
+            var matchteamsdataraw = wc.DownloadString(matchteamsurl);
+            var matchteamsdata = JsonConvert.DeserializeObject<List<HeroesLoungeMatchHandler.Team>>(matchteamsdataraw, settings);
 
+            /* setting the Teams*/
             teamLeft = matchteamsdata[0].title;
             teamRight = matchteamsdata[1].title;
 
+            /* generate team-logos */
             String urlTeamPicLeft = "https://heroeslounge.gg/api/v1/teams/" + matchteamsdata[0].id + "/logo";
             String urlTeamPicRight = "https://heroeslounge.gg/api/v1/teams/" + matchteamsdata[1].id + "/logo";
 
+            /* download and convert team-logos */
             var teamPicLeft = JsonConvert.DeserializeObject<HeroesLoungeMatchHandler.Picture>(wc.DownloadString(urlTeamPicLeft), settings);
             var teamPicRight = JsonConvert.DeserializeObject<HeroesLoungeMatchHandler.Picture>(wc.DownloadString(urlTeamPicRight), settings);
-            //wc.DownloadFile(new Uri(teamPicLeft.path), @"C:/teamLeft.png");
-            //wc.DownloadFile(new Uri(teamPicRight.path), @"teamRight.png");
             wc.DownloadFile(new Uri(teamPicLeft.path), path + @"teamLeft.png");
             wc.DownloadFile(new Uri(teamPicRight.path), path + @"teamRight.png");
         }
